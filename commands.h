0848ffb85efbf763811d437f9efb88ab9d10e330
Robin Dietzel <robtor2000@web.de>
2019-03-21 23:27:53 +0100

    changed var

--- commands.h
+++ commands.h
@@ -1,14 +1,15 @@
+ String split_res [5];
+
 void split_string(String data) {
- String split_res[];
  int i = 0;
 
- int val = indexOf(':');
+ int val = data.indexOf(':');
 
  while(val != -1) {
    split_res[i] = data.substring(0,val-1);
    data = data.substring(val+1);
    i++;
-   int val = indexOf(':');
+   int val = data.indexOf(':');
  }
 
  split_res[i] = data;
