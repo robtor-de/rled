29ed2615e3be164953dc336bad7bcb76c05482f7
Robin Dietzel <robtor2000@web.de>
2019-03-21 23:16:02 +0100

    split

--- commands.h
+++ commands.h
@@ -1,7 +1,13 @@
-void split_string(String *data) {
- int val = 0;
+void split_string(String data) {
+ String split_res[];
+ int i = 0;
+
+ int val = indexOf(':');
 
  while(val != -1) {
+   split_res[i] = data.substring(0,val-1);
+   data = data.substring(val+1);
+   i++;
    
  }
 }
