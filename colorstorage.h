4973acc1f2f50eca8aab5b3db8c8cea1c3be4b78
Robin Dietzel <robtor2000@web.de>
2019-03-24 14:11:11 +0100

    added EEPROM code

--- colorstorage.h
+++ colorstorage.h
@@ -1,1 +1,11 @@
-color startupcolor;
+void save_startup_color(color startupcolor) {
+  EEPROM.put(0,startupcolor);
+}
+
+void load_startup_color() {
+  color startupcolor;
+
+  EEPROM.get(0,startupcolor);
+
+  set_rgb(startupcolor);
+}
