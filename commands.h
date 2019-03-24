092c2353a240a98fb95ed34e1f019ec45c0db990
Robin Dietzel <robtor2000@web.de>
2019-03-24 14:01:11 +0100

    changed command interpreter

--- commands.h
+++ commands.h
@@ -1,24 +1,12 @@
- String split_res [5];
+color temp_color;
 
-void split_string(String data) {
- int i = 0;
-
- int val = data.indexOf(':');
-
- while(val != -1) {
-   split_res[i] = data.substring(0,val-1);
-   data = data.substring(val+1);
-   i++;
-   int val = data.indexOf(':');
- }
-
- split_res[i] = data;
-}
-
-void colorparse(String colordata) {
-  
+void parsecolor(String col) {
+  temp_color.red = col.substring(0,2).toInt();
+  temp_color.green = col.substring(3,5).toInt();
+  temp_color.blue = col.substring(6,8).toInt();
 }
 
 void c_set(String *tail) {
-
+  parsecolor(*tail);
+  set_rgb(temp_color);
 }
