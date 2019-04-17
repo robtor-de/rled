d7c35c82e958044549cb42980c2841e6f93108c8
Robin Dietzel <robtor2000@web.de>
2019-04-17 13:04:03 +0200

    fixex color parser error

--- commands.h
+++ commands.h
@@ -1,9 +1,9 @@
 color temp_color;
 
 void parsecolor(String col) {
-  temp_color.red = col.substring(0,2).toInt();
-  temp_color.green = col.substring(3,5).toInt();
-  temp_color.blue = col.substring(6,8).toInt();
+  temp_color.red = col.substring(0,3).toInt();
+  temp_color.green = col.substring(3,6).toInt();
+  temp_color.blue = col.substring(6,9).toInt();
 }
 
 void c_set(String *tail) {
